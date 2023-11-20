73ff38cbf7908d3819cac9965983b73b8b470289
Алексей Шульга <shulga.alexey.00@yandex.ru>
2023-11-20 23:45:43 +0300

    Fix main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+"""Отвечает за запуск примеров заданий."""
 from sys import argv
-from hw5 import Task1, Task3, Task4, Task5, Task7, Task8
+from hw5 import Task1, Task2, Task3, Task4, Task5, Task7, Task8
 
 if argv[1] == '5':
     if argv[2] == '1':
@@ -8,17 +9,20 @@ if argv[1] == '5':
         elif argv[3] == 'b':
             Task1(13, 17, 3,  'Task1 (B): g=13, M=17, m=3', 100, 0, 1)
     elif argv[2] == '2':
-        pass
+        Task2(12, 1/3, 'Task2 : Bi(12, 0.33)', 12, 0, 12)
     elif argv[2] == '3':
         Task3(10, 0.1,  'Task3 : Pascal(10, 0.1)', 190, 10, 200)
     elif argv[2] == '4':
         Task4(5, 0.1,  'Task4 : Poisson(5, 0.1)', 20, 0, 20)
     elif argv[2] == '5':
-        pass
+        if argv[3] == 'a':
+            Task5(10**6, 'Task5 (A): {M*gamma} 10^6', 100, 0, 10**6)
+        elif argv[3] == 'b':
+            Task5(10**7, 'Task5 (B): {M*gamma} 10^7', 100, 0, 10**7)
     elif argv[2] == '6':
         pass
     elif argv[2] == '7':
-        pass
+        Task7(50, 1/3, 'Task7 : Bi(50, 0.33)', 25, 5, 30)
     elif argv[2] == '8':
         if argv[3] == 'a':
             Task8(10, 'Task8 (A): Poisson(lambda=10)', 25, 0, 25)
